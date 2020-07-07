0ab26be0dc9591a4c8ac89c75023bf2667310561
martinbeck723 <xishuoliu514@gmail.com>
2020-07-07 09:13:35 -0400

    add some ui

--- app/src/main/java/com/raywenderlich/listmaker3/ListSelectionRecyclerViewAdapter.kt
+++ app/src/main/java/com/raywenderlich/listmaker3/ListSelectionRecyclerViewAdapter.kt
@@ -3,17 +3,22 @@ package com.raywenderlich.listmaker3
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class ListSelectionRecyclerViewAdapter :
-    RecyclerView.Adapter<ListSelectionViewHolder>() {
+class ListSelectionRecyclerViewAdapter : RecyclerView.Adapter<ListSelectionViewHolder>() {
+
+    //mock titles
+    val listTitles = arrayOf("Shopping List", "Chores", "Android Tutorials")
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListSelectionViewHolder {
         TODO("Not yet implemented")
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+
 
     override fun onBindViewHolder(holder: ListSelectionViewHolder, position: Int) {
         TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        return listTitles.size
     }
 }
